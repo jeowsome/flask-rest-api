c62d7b233936e2351542b3593caa59ad73f07029
jeowsome <jeowsome15@gmail.com>
2023-05-23 14:08:36 +0800

    updated tags endpoint

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ import models
 
 from resources.item import blp as ItemBlueprint
 from resources.store import blp as StoreBlueprint
+from resources.tag import blp as TagBlueprint
 
 
 def create_app(db_url=None):
@@ -17,17 +18,22 @@ def create_app(db_url=None):
     app.config["OPENAPI_VERSION"] = "3.0.3"
     app.config["OPENAPI_URL_PREFIX"] = "/"
     app.config["OPENAPI_SWAGGER_UI_PATH"] = "/swagger-ui"
-    app.config["OPENAPI_SWAGGER_UI_URL"] = "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
-    app.config["SQLALCHEMY_DATABASE_URI"] = db_url or os.getenv("DATABASE_URL", "sqlite:///data.db")
-    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False # not necessary
+    app.config[
+        "OPENAPI_SWAGGER_UI_URL"
+    ] = "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
+    app.config["SQLALCHEMY_DATABASE_URI"] = db_url or os.getenv(
+        "DATABASE_URL", "sqlite:///data.db"
+    )
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False  # not necessary
     db.init_app(app)
 
     api = Api(app)
-    
-    with app.app_context(): 
-        db.create_all() # runs only if the table doesn't exists
-    
+
+    with app.app_context():
+        db.create_all()  # runs only if the table doesn't exists
+
     api.register_blueprint(ItemBlueprint)
     api.register_blueprint(StoreBlueprint)
-    
+    api.register_blueprint(TagBlueprint)
+
     return app
